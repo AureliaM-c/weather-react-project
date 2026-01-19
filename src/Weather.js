dd4754a617ae51d4b6325d1790404485ecd90f70
Aurelia <m_aurelia@hotmail.fr>
2026-01-19 14:50:56 +0000

    added Weather component css

--- src/Weather.js
+++ src/Weather.js
@@ -11,10 +11,15 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city.."
               className="form-control"
+              autoFocus="true"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -23,14 +28,20 @@ export default function Weather() {
         <li>Monday 07:00</li>
         <li>Mostly Cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="partlycloudy"
-          />
-          6°C
+          <div className="weather-temperature">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="partlycloudy"
+            />
+            <div className="temperature-container">
+              <span className="temperature">6</span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
         </div>
+
         <div className="col-6">
           <ul>
             <li>Humidity: 80%</li>
