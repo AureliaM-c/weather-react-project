4cbb1325915c00d80c41284d29cc6afdd85b51af
Aurelia <m_aurelia@hotmail.fr>
2026-01-19 12:38:11 +0000

    added Git repo URL and Git profile URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,29 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather React Project</div>;
+  return (
+    <div className="App">
+      <h1> Weather React Project</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/AureliaM-c"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Aurelia Mourey
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/AureliaM-c/weather-react-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
