1e65b632da427078b7ff1083660f04a3986c49c5
Aurelia <m_aurelia@hotmail.fr>
2026-01-19 13:34:58 +0000

    created weather component and added component structure

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
 import React from "react";
-
+import Weather from "./Weather";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1> Weather React Project</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://github.com/AureliaM-c"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Aurelia Mourey
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/AureliaM-c/weather-react-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/AureliaM-c"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Aurelia Mourey
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/AureliaM-c/weather-react-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
