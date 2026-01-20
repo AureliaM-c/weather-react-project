66635deccac12773137fb4b124c05d5910135b8b
Aurelia <m_aurelia@hotmail.fr>
2026-01-20 17:34:12 +0000

    installed axios and created weather data function + API integration

--- src/App.js
+++ src/App.js
@@ -5,28 +5,26 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <Weather />
+      <Weather defaultCity="New York" />
 
-        <footer>
-          This project was coded by{" "}
-          <a
-            href="https://github.com/AureliaM-c"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Aurelia Mourey
-          </a>{" "}
-          and is{" "}
-          <a
-            href="https://github.com/AureliaM-c/weather-react-project"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open-sourced on Github
-          </a>
-        </footer>
-      </div>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/AureliaM-c"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Aurelia Mourey
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/AureliaM-c/weather-react-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -5,11 +5,12 @@ import "./Weather.css";
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
   function handleResponse(response) {
+    console.log(response.data);
     setWeatherData({
       ready: true,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
-      date: "Monday 07:00",
+      date: "Wednesday 10:00",
       description: response.data.weather[0].description,
       iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
       wind: response.data.wind.speed,
@@ -20,54 +21,64 @@ export default function Weather(props) {
   if (weatherData.ready) {
     return (
       <div className="weather">
+        {" "}
         <form>
+          {" "}
           <div className="row">
+            {" "}
             <div className="col-9">
+              {" "}
               <input
                 type="search"
                 placeholder="Enter a city.."
                 className="form-control"
                 autoFocus="true"
-              />
-            </div>
+              />{" "}
+            </div>{" "}
             <div className="col-3">
+              {" "}
               <input
                 type="submit"
                 value="Search"
                 className="btn btn-primary w-100"
-              />
-            </div>
-          </div>
+              />{" "}
+            </div>{" "}
+          </div>{" "}
         </form>
-        <h1>{weatherData.city}</h1>
+        <h1>{weatherData.city}</h1>{" "}
         <ul>
-          <li>Monday 07:00</li>
-          <li>Mostly Cloudy</li>
-        </ul>
+          {" "}
+          <li>{weatherData.date}</li>
+          <li className="text-capitalize">{weatherData.description}</li>{" "}
+        </ul>{" "}
         <div className="row mt-3">
+          {" "}
           <div className="col-6">
+            {" "}
             <div className="weather-temperature">
+              {" "}
               <img
                 src={weatherData.iconUrl}
                 alt={weatherData.description}
                 className="weather-icon"
-              />
+              />{" "}
               <div className="temperature-container">
+                {" "}
                 <span className="temperature">
-                  {Math.round(weatherData.temperature)}
+                  {Math.round(weatherData.temperature)}{" "}
                 </span>
-                <span className="unit">°C</span>
-              </div>
-            </div>
-          </div>
-
+                <span className="unit">°C</span>{" "}
+              </div>{" "}
+            </div>{" "}
+          </div>{" "}
           <div className="col-6">
+            {" "}
             <ul>
-              <li>Humidity: {weatherData.humidity}%</li>
-              <li>Wind: {weatherData.wind}</li>
-            </ul>
-          </div>
-        </div>
+              <li>Humidity: {weatherData.humidity}%</li>{" "}
+              <li>Wind: {weatherData.wind}</li>{" "}
+            </ul>{" "}
+          </div>{" "}
+        </div>{" "}
       </div>
     );
   } else {
