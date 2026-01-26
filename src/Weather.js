ad2165f530a7016d499991c5548c3add6b562637
Aurelia <m_aurelia@hotmail.fr>
2026-01-26 10:54:23 +0000

    update apiurl for better netlify deploy

--- src/Weather.js
+++ src/Weather.js
@@ -21,7 +21,7 @@ export default function Weather(props) {
 
   function search() {
     const apiKey = "1ee4264117b73d2263eecd562f31ef5c";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
 
